e4fb62eb0bb01e219dcb0fc651742834ada05cfd
Sameh Bilal <sameh.bilal@outlook.com>
2021-12-14 16:57:49 +0200

    updaters

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function index()
+    {
+        $user = User::find(auth()->user()->id);
+        return view('setup.settings.profile',compact('user'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+
+        $this->validate($request, User::rules($update = true, $user->id));
+
+        $user->update([
+            'first_name'            => $request->first_name,
+            'last_name'             => $request->last_name,
+            'full_name'             => $request->full_name,
+            'email'                 => $request->email,
+            'other_email'           => $request->other_email,
+            'username'              => $request->username,
+            'password'              => $request->password == '' ? $user->password:$request->password,
+            'gender'                => $request->gender,
+            'religion'              => $request->religion,
+            'date_of_birth'         => $request->date_of_birth,
+            'phone'                 => $request->phone,
+            'other_phone'           => $request->other_phone,
+            'bio'                   => $request->bio,
+        ]);
+
+        return redirect()->back()->with('success','Data updated successfully');
+    }
+}
